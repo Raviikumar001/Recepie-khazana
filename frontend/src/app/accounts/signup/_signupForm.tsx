3d09d309bdbd2fbb31b8cd323c60cdb33a3ae033
Ravi <samcollins9899@gmail.com>
2024-01-12 10:06:10 +0530

    added login page

---
+++ frontend/src/app/accounts/signup/_signupForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SignupForm = () => {
+    return (
+        <div>
+            <form>
+                <label htmlFor='email'></label><br />
+                <input name='email' />
+                <label htmlFor='password' /> <br />
+                <button >Sign In</button>
+
+
+
+            </form>
+
+
+        </div>
+    )
+}
+
+export default SignupForm
--- frontend/src/app/accounts/signup/signup.tsx
+++ frontend/src/app/accounts/signup/signup.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import pizza from '../../../../public/pizza.jpg'
+
 
 const Signup = () => {
+
+
+
+
   return (
-    <div>
-      
+    <div className='grid grid-col-2 grid-flow-row  h-full'>
+      <div
+      style={{
+        backgroundImage: 'url("/pizza.jpg")',
+        backgroundSize: 'cover',
+        height: '100vh',
+        width: "100%"
+      }}
+    >
+            
+      </div>
+      <div>
+
+        <h1>Receipe Khazna</h1>
+        <h3>Sign in or create an account</h3>
+
+      </div>
+    
     </div>
   )
+
 }
 
 export default Signup

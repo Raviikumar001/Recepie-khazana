70d6efb5aa6d15a4cbcdf9ecdfcfdb4b46e85fd6
Ravi <samcollins9899@gmail.com>
2024-01-20 21:02:15 +0530

    added es lint rule

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
 
     reactStrictMode: false,
+
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 }
 
 module.exports = nextConfig

2a25e554d5abfe27e649442725cb421b385df74e
Ravi <samcollins9899@gmail.com>
2024-01-22 12:27:14 +0530

    modifed login route

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -4,41 +4,79 @@ const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
 
-const login =async (req, res, next)=> 
-{
+
+const login = async (req, res, next) => {
   console.log('hi');
   try {
-      const {email, password} = req.body;
-      
-      const user = await User.findOne({email: email});
-      console.log(user)
-      if(!user)
-      {
-        res.status(401).json({message: "User Does not Exists"});
+    const { email, password } = req.body;
 
-      }else if(user)
-      {
-        const passwordMatch= await bcrypt.compare(password, user.password);
-        console.log(passwordMatch)
-        if(!passwordMatch)
-        {
-          res.status(401).json({message:"Password is incorrect"});
-        }
-        const userWithoutPassword = { ...user.toObject(), password: undefined };
-       
-        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-          expiresIn: '1h',
-          });
-        
-        res.status(200).json({token,user: userWithoutPassword, message:"User Authenticated"});
-      }
+    const user = await User.findOne({ email: email });
+    console.log(user);
 
+    if (!user) {
+      return res.status(401).json({ message: "User Does not Exist" });
+    }
 
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    console.log(passwordMatch);
 
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Password is incorrect" });
+    }
+
+    const userWithoutPassword = { ...user.toObject(), password: undefined };
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: '1h',
+    });
+
+    return res.status(200).json({
+      token,
+      user: userWithoutPassword,
+      message: "User Authenticated",
+    });
   } catch (error) {
-    res.status(500).json({error: 'Login failed'})
+    console.error(error);
+    return res.status(500).json({ error: 'Login failed' });
   }
-}
+};
+
+
+// const login =async (req, res, next)=> 
+// {
+//   console.log('hi');
+//   try {
+//       const {email, password} = req.body;
+      
+//       const user = await User.findOne({email: email});
+//       console.log(user)
+//       if(!user)
+//       {
+//         res.status(401).json({message: "User Does not Exists"});
+
+//       }else if(user)
+//       {
+//         const passwordMatch= await bcrypt.compare(password, user.password);
+//         console.log(passwordMatch)
+//         if(!passwordMatch)
+//         {
+//           res.status(401).json({message:"Password is incorrect"});
+//         }
+//         const userWithoutPassword = { ...user.toObject(), password: undefined };
+       
+//         const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+//           expiresIn: '1h',
+//           });
+        
+//         res.status(200).json({token,user: userWithoutPassword, message:"User Authenticated"});
+//       }
+
+
+
+//   } catch (error) {
+//     res.status(500).json({error: 'Login failed'})
+//   }
+// }
 
 
 const register = async (req, res) => {
